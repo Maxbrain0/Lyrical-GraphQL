c6d3cd9644240a25f4e0b8289cf920f3c8711b50
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-11-13 17:30:52 -0800

    Add react router

---
+++ client/components/SongList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => (
+      <li key={song.id} className="collection-item">
+        {song.title}
+      </li>
+    ));
+  }
+  render() {
+    return this.props.data.loading ? (
+      <div>Loading songs...</div>
+    ) : (
+      <ul className="collection">{this.renderSongs()}</ul>
+    );
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
--- client/index.js
+++ client/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import App from './components/App';
+import SongList from './components/SongList';
+
+// looking for default  :4000/graphql
 const client = new ApolloClient({});
 
 const Root = () => (
   <ApolloProvider client={client}>
-    <div>Lyrical</div>
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={SongList} />
+      </Route>
+    </Router>
   </ApolloProvider>
 );
 ReactDOM.render(<Root />, document.querySelector('#root'));
